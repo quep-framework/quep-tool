a6ea874ba542755f22bd30589c3e399892538f7b
agna8685 <agna8685@192.168.1.112>
2017-01-23 12:59:16 +0100

    23/01/2017 last11

--- src/main/java/es/upv/dsic/quep/beans/QuestionnaireDynamicBean.java
+++ src/main/java/es/upv/dsic/quep/beans/QuestionnaireDynamicBean.java
@@ -107,7 +107,7 @@ public class QuestionnaireDynamicBean implements Serializable {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        //Test Oscar
+        //Test Agna
     }
 
     public void buildQuestionnaire() {
